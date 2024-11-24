175c5c2e2ee2db8b7f431818cccbd7be6bce159e
unknown <joshuadiasbf@gmail.com>
2024-11-24 10:57:50 -0300

    Start Activity actions

--- app/src/main/java/com/br/condoconnect_mobile/RegisterActivity.kt
+++ app/src/main/java/com/br/condoconnect_mobile/RegisterActivity.kt
@@ -1,28 +1,20 @@
 package com.br.condoconnect_mobile
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        loginButton.setOnClickListener {
-            val intent = Intent(this, StartActivity::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/br/condoconnect_mobile/StartActivity.kt
+++ app/src/main/java/com/br/condoconnect_mobile/StartActivity.kt
@@ -1,16 +1,29 @@
 package com.br.condoconnect_mobile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_start)
+
+        // Configuração do listener para o botão 'carrinhoLogin'
+        val carrinhoLogin: ImageButton = findViewById(R.id.carrinhoLogin)
+
+        carrinhoLogin.setOnClickListener {
+            // Criando o Intent para abrir a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Configuração do padding para Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
