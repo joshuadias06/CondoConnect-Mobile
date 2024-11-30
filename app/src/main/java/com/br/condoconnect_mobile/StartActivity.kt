1c32cb4edff195a36d05b98e32f421d056102ba4
unknown <joshuadiasbf@gmail.com>
2024-11-30 18:36:16 -0300

    Butons StartActivity

--- app/src/main/java/com/br/condoconnect_mobile/StartActivity.kt
+++ app/src/main/java/com/br/condoconnect_mobile/StartActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +23,30 @@ class StartActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+        // Configuração do listener para o botão 'carrinho'
+        val carrinho: ImageView = findViewById(R.id.carrinho)
+        carrinho.setOnClickListener {
+            // Criando o Intent para abrir a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+// Configuração do listener para o botão 'agendar'
+        val agendar: ImageView = findViewById(R.id.agendar)
+        agendar.setOnClickListener {
+            // Criando o Intent para abrir a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+// Configuração do listener para o botão 'config'
+        val config: ImageView = findViewById(R.id.config)
+        config.setOnClickListener {
+            // Criando o Intent para abrir a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
 
         // Configuração do padding para Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
